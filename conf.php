10342d43ba5d1937a84ebc4e1534e84cad171b1e
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-18 09:43:30 +0200

    Konfiguratsiooni määratud abikonstandid

---
+++ conf.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tarvi.liivak
+ * Date: 18.01.2018
+ * Time: 9:36
+ */
+// konfiguratsiooni fail
+
+// loome vajalikud abikonstandid
+define('MODEL_DIR', 'model/');
+define('VIEWS_DIR', 'views/');
+define('CONTROL_DIR', 'controllers/');
