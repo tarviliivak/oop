2ae7dc30789e139400f0cddf3cffe7d394e87363
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-18 10:40:39 +0200

    HTML vaade faili sisu lugemine on kirjeldatud

--- model/template.php
+++ model/template.php
@@ -16,5 +16,13 @@ class template
     // reaalsed väärtused html vaade sablooni
     // täitmiseks
     var $vars = array();
+    // template klassi meetodid
+    // html vaade faili sisu lugemine
+    function readFile($f){
+        //$fp = fopen($f, 'rb');
+        //$this->content = fread($fp, filesize($f));
+        //fclose($fp);
+        $this->content = file_get_contents($f);
+    }
 
 }
