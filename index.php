64e0957a7f9d61e312a23440434039178cf4d601
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-19 14:49:23 +0200

    Reaalsed väärtused määratud

--- index.php
+++ index.php
@@ -8,7 +8,15 @@
 require_once 'conf.php';
 
 $mainTmpl = new template('main');
-
+//
+$mainTmpl->set('site_lang', 'et');
+$mainTmpl->set('site_title', 'PV');
+$mainTmpl->set('user', 'Kasutaja');
+$mainTmpl->set('title', 'Pealkiri');
+$mainTmpl->set('lang_bar', 'Keeleriba');
+$mainTmpl->set('menu', 'lehe menüü');
+$mainTmpl->set('content', 'Lehe sisu');
+//väljastame objekti sisu test kujul
 echo '<pre>';
 print_r($mainTmpl);
 echo '</pre>';
