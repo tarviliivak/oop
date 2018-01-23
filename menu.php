7fc763ec217b7a932f485396594ac3d41ad93f18
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-23 14:37:39 +0200

    Et oleks võimalik lisada menusse esimene ja teine korraga

--- menu.php
+++ menu.php
@@ -6,4 +6,24 @@
  * Time: 14:13
  */
 
-$mainTmpl->set('menu', 'lehe menüü');
+// loome menüü ehitamiseks vajalikud objektid
+$menuTmpl = new template('menu.menu'); //menüü mall
+$itemTmpl = new template('menu.item'); //item mall
+
+$itemTmpl->set('name', 'esimene');
+// lisame antud elemendi menüüsse
+$menuItem = $itemTmpl->parse(); // string mis sisaldab ühe nimekirja elemente
+$menuTmpl->add('menu_items', $menuItem); //
+
+
+$itemTmpl->set('name', 'teine');
+// lisame antud elemendi menüüsse
+$menuItem = $itemTmpl->parse(); // string mis sisaldab ühe nimekirja elemente
+$menuTmpl->add('menu_items', $menuItem); //
+
+// ehitame valmis menüü
+$menu = $menuTmpl->parse();
+
+
+// lisame valmis menüü lehele
+$mainTmpl->set('menu', $menu);
