16893cbcd3ab4eca24bb581f00735097bfbff9c9
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-24 11:04:18 +0200

    Menüü on täiendatud linkidega

--- menu.php
+++ menu.php
@@ -5,18 +5,25 @@
  * Date: 23.01.2018
  * Time: 14:13
  */
-
+//määrame menüüd väljastades elemendi nime
 // loome menüü ehitamiseks vajalikud objektid
 $menuTmpl = new template('menu.menu'); //menüü mall
 $itemTmpl = new template('menu.item'); //item mall
 
 $itemTmpl->set('name', 'esimene');
 // lisame antud elemendi menüüsse
+// http://tl.ikt.khk.ee/oop/index.php/control/esimene
+$link = $http->getLink(array('control'=>'esimene'));
+$itemTmpl->set('link', $link);
+//
 $menuItem = $itemTmpl->parse(); // string mis sisaldab ühe nimekirja elemente
 $menuTmpl->add('menu_items', $menuItem); //
 
 
 $itemTmpl->set('name', 'teine');
+//
+$link = $http->getLink(array('control'=>'teine'));
+$itemTmpl->set('link', $link);
 // lisame antud elemendi menüüsse
 $menuItem = $itemTmpl->parse(); // string mis sisaldab ühe nimekirja elemente
 $menuTmpl->add('menu_items', $menuItem); //
