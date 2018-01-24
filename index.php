30c4b97ded8046ae0650b38675663b7266df029d
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-24 09:31:23 +0200

    Serveri konstantide kontroll on teostatud

--- index.php
+++ index.php
@@ -5,20 +5,20 @@
  * Date: 19.01.2018
  * Time: 14:18
  */
+// nõuame konfiguratsiooni faili
 require_once 'conf.php';
-
+// loome peamalli objekti
 $mainTmpl = new template('main');
-//
+// reaalväärtuste määramine
 $mainTmpl->set('site_lang', 'et');
 $mainTmpl->set('site_title', 'PV');
 $mainTmpl->set('user', 'Kasutaja');
 $mainTmpl->set('title', 'Pealkiri');
 $mainTmpl->set('lang_bar', 'Keeleriba');
+// lisame menüü failist
 require_once 'menu.php';
 $mainTmpl->set('content', 'Lehe sisu');
-//väljastame objekti sisu test kujul
-echo '<pre>';
-print_r($mainTmpl);
-echo '</pre>';
-// väljastame sisuga täiedtud malli
+// väljastame sisuga täidetud mall
 echo $mainTmpl->parse();
+// kontrollime $http objekti tööd
+echo HTTP_HOST.SCRIPT_NAME.'<br />';
