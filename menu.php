64bee80151ad7ce543309e550896a329abd8d794
tarvi.liivak <tarvi.liivak@khk.ee>
2018-01-31 11:27:04 +0200

    Lisame menüüsse lingi nimega avaleht

--- menu.php
+++ menu.php
@@ -10,6 +10,16 @@
 $menuTmpl = new template('menu.menu'); //menüü mall
 $itemTmpl = new template('menu.item'); //item mall
 
+$itemTmpl->set('name', 'avaleht');
+// lisame antud elemendi menüüsse
+// http://tl.ikt.khk.ee/oop/index.php/control/avaleht
+$link = $http->getLink(array('control'=>'avaleht'));
+$itemTmpl->set('link', $link);
+//
+$menuItem = $itemTmpl->parse(); // string mis sisaldab ühe nimekirja elemente
+$menuTmpl->add('menu_items', $menuItem);
+//
+//
 $itemTmpl->set('name', 'esimene');
 // lisame antud elemendi menüüsse
 // http://tl.ikt.khk.ee/oop/index.php/control/esimene
