36ee6ac143df5ed27b63d15d07f6c45e958fea3a
tarvi.liivak <tarvi.liivak@khk.ee>
2018-02-02 09:17:24 +0200

    konf fail

--- conf.php
+++ conf.php
@@ -23,4 +23,4 @@ require_once MODEL_DIR.'template.php'; // html vaade failide töötlus
 require_once MODEL_DIR.'http.php'; // HTTP töötlus klass
 require_once MODEL_DIR.'linkobjekt.php'; // Lingi töötluse klass
 // loome vajalikud objektid, mis on pidevalt tööl
-$http = new linkobject();
+require_once MODEL_DIR.'mysql.php'; // DB töötlus klass
