063e3e0dd7374d0981e529e96302006029df6750
tarvi.liivak <tarvi.liivak@khk.ee>
2018-02-12 10:25:24 +0200

    Failide esimene ja teine kustutamine

--- controllers/esimene.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: tarvi.liivak
- * Date: 31.01.2018
- * Time: 10:25
- */
-$mainTmpl->set('content', '1. lehe sisu');
--- controllers/teine.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: tarvi.liivak
- * Date: 31.01.2018
- * Time: 10:48
- */
-$mainTmpl->set('content', '2. lehe sisu');
